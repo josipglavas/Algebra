336be50fa62de5bd08dcad344be0bc21277a10fe
josipglavas <glavas.josip@icloud.com>
2025-03-17 19:42:44 +0100

    Zavrsen program koji ispusuje sve Armstrongove brojeve manje od 10000

---
+++ Vjezba1/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 10000; i++){
+            int armstrongovBroj = 0;
+            int zbrojZnamenki = 0;
+            if(i > 0){
+                int znamenka = 0;
+                int brojZnamenki = 1;
+                if(i > 10 && i < 100){
+                    brojZnamenki = 2;
+                } else if (i >= 100 && i < 1000 ){
+                    brojZnamenki = 3;
+                } else if (i>=1000 ) {
+                    brojZnamenki = 4;
+                }
+                int broj = i;
+                for(int j = 0; j < brojZnamenki; j++){
+                    znamenka = ( broj % 10);
+                    broj = broj / 10;
+                    if(znamenka > 0){
+                        int ostatak = broj % znamenka;
+                        if (ostatak > 0 && brojZnamenki == 1){
+                            broj = ostatak;
+                        }
+                        zbrojZnamenki += (int)Math.pow(znamenka, 3);
+                    }
+                }
+                if(zbrojZnamenki == i){
+                    armstrongovBroj = i;
+                }
+            }
+            if(armstrongovBroj > 0) System.out.println(armstrongovBroj);
+        }
+    }
+}
