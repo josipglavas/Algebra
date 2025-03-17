eda4d66c47da85aa7f98bcd688c9e3a30325fa6e
josipglavas <glavas.josip@icloud.com>
2025-03-17 20:10:25 +0100

    Program koji ispusuje sve Armstrongove brojeve manje od 10000.

--- Vjezba1/Main.java
+++ Vjezba1/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
         for(int i = 0; i < 10000; i++){
             int armstrongovBroj = 0;
             int zbrojZnamenki = 0;
